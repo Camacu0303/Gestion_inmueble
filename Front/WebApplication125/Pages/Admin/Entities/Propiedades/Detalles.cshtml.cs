fe1318b523e391821e92c819ecc1472f15852f69
ManuelBaezSanabria <manuelbsanabria@gmail.com>
2025-04-19 22:06:37 -0600

    Vista de propiedades en el index, así como visualización a detalles

---
+++ Front/WebApplication125/Pages/Admin/Entities/Propiedades/Detalles.cshtml.cs
@@ -0,0 +1,34 @@
+using API_WIN_MAIN.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WEB.Models;
+using WEB.Util;
+
+namespace WEB.Pages.Admin.Entities.Propiedades
+{
+    public class DetallesModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public DetallesModel(IHttpClientFactory httpClient, ILogger<IndexModel> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public Propiedad? Propiedad { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var client = _httpClient.CreateClient("ApiClient");
+            Propiedad = await EntityService.GetByIdAsync<Propiedad>(client, id, "Propiedades");
+
+            if (Propiedad == null)
+                return NotFound();
+
+            return Page();
+        }
+    }
+}
--- Front/WebApplication125/Pages/Index.cshtml.cs
+++ Front/WebApplication125/Pages/Index.cshtml.cs
@@ -1,12 +1,51 @@
+using API_WIN_MAIN.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net.Http;
 using System.Text.Json;
+using WEB.Util;
 
 namespace WebApplication125.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IHttpClientFactory _httpClient;
+
+        public IndexModel(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [BindProperty]
+        public Propiedad Propiedad { get; set; } = default!;
+
+        [BindProperty]
+        public IFormFile? ImagenFile { get; set; }
+
+        public List<EstadoPropiedad>? EstadosPropiedad { get; set; }
+        public List<TipoPropiedad>? TiposPropiedad { get; set; }
+        public List<Usuario>? Usuarios { get; set; }
+        public List<Propiedad>? Propiedades { get; set; }
+        public string? ApiError { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            var client = _httpClient.CreateClient("ApiClient");
+            Propiedades = await EntityService.GetAllAsync<Propiedad>(client, "Propiedades");                
+
+            EstadosPropiedad = await EntityService.GetAllAsync<EstadoPropiedad>(client, "EstadoPropiedad");
+            TiposPropiedad = await EntityService.GetAllAsync<TipoPropiedad>(client, "TipoPropiedad");
+            Usuarios = await EntityService.GetAllAsync<Usuario>(client, "Usuario");
+            Propiedades = await EntityService.GetAllAsync<Propiedad>(client, "Propiedades");
+
+            if (Propiedades == null || !Propiedades.Any())
+                ApiError = "No se encontraron propiedades.";
+
+            
+        }
+
+
 
     }
 }
